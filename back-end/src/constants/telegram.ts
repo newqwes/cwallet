90e1fea5c42fb802f35eb509d6a9fc6b877404c8
newqwes <newnewqwes@gmail.com>
2024-06-10 23:37:26 +0200

    #14. Check: referral link
    Signed-off-by: newqwes <newnewqwes@gmail.com>

--- back-end/src/constants/telegram.ts
+++ back-end/src/constants/telegram.ts
@@ -2,12 +2,13 @@ import { WEB_APP_URL } from "./index";
 
 export const RUN_APP = 'Run 🪿';
 
+console.log('--QWES-- WEB_APP_URL: ', WEB_APP_URL);
 export const getMessageOptions = (refCode?: string) => ({
   parse_mode: 'markdown',
   reply_markup: {
     resize_keyboard: true,
     inline_keyboard: [
-      [{text: RUN_APP, web_app: {url: WEB_APP_URL + refCode ? `?refCode=${refCode}` : ''}}]
+      [{text: RUN_APP, web_app: {url: WEB_APP_URL + (refCode ? `?refCode=${refCode}` : '')}}]
     ]
   },
 });
